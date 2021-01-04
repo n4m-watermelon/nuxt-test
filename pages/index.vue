70af83003736f51a5c7b18d9548ce2ceb85de031
le doan nam <namthamlam505@gmail.com>
2021-01-04 10:18:34 +0700

    UP TEST

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="container">
-    <div>
+    <!-- <div>
         <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
         <nuxt-link :to="switchLocalePath('fr')">Français</nuxt-link>
         <h1>{{$t('welcome')}}</h1>
-    </div>
+    </div> -->
+    <Test />
 </div>
 </template>
 
